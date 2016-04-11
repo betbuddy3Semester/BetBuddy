5de112b9682166331ba378d0bfc9c2195a8845e9
Alex-Pedersen <1038281@ucn.dk>
2016-04-11 21:18:52 +0200

    Initial Commit of Solution

---
+++ BetBud/BetBudTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace BetBudTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //Arrange
+            var one = 1;
+            var two = 2;
+
+            //Act
+            var resul = one + two;
+
+            //Assure
+            Assert.AreEqual(resul, 3);
+        }
+    }
+}
