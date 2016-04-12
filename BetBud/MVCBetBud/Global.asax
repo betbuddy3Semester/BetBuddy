81848266861c31d03eebdab629be6b14c1efb91b
Alex-Pedersen <1038281@ucn.dk>
2016-04-12 09:32:16 +0200

    Creating wcf and mvc

---
+++ BetBud/MVCBetBud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCBetBud.MvcApplication" Language="C#" %>
---
+++ BetBud/MVCBetBud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBetBud.Startup))]
+namespace MVCBetBud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
