bfe86bec82087d374717ab80d38d984145da8831
Reetzer <318956@gmail.com>
2016-04-12 12:35:58 +0200

    Oprettelse af Interface: Kupon, Kampe og DelKampe

---
+++ BetBud/ModelLibrary/IDelKamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Emit;
+
+namespace ModelLibrary
+{
+    public interface IDelKamp
+    {
+        bool Valgt1 { get; set; }
+        bool ValgtX { get; set; }
+        bool Valgt2 { get; set; }
+        
+        IKampe Kampe { get; set; }
+
+        bool KampRigtig();
+
+        double GetOdds();
+
+    }
+
+}
---
+++ BetBud/ModelLibrary/IKupon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelLibrary
+{
+    public interface IKupon
+    {
+        /**
+        * Oprettelse af kupon
+        */
+        void TilføjKamp(IKampe kamp, bool valgt1, bool valgtX, bool valgt2);
+        void FjernKamp(IKampe kamp);
+        double OddsUdregning();
+        double MuligGevist();
+        bool BekræftKupon();
+        
+        /**
+        * Kontrol/indløsning af kupon
+        */
+        bool KontrolAfKupon();
+        bool Kontrolleret { get; }
+        double Point { get; set; }
+        List<IDelKamp> DelKampe { get; set; }
+        IBruger Bruger { get; set; }
+        
+        
+        
+
+
+        
+    }
+}
