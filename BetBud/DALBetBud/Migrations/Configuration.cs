439e8db1009236bda1d4e95bf3444422ae2ec16d
Nickolai <Nickolai.madsen93@gmail.com>
2016-04-13 12:06:39 +0200

    Ændringer i Context, MigrationConfig, WFC App.config, WFC BrugerService, NuGet Entity package installeret

---
+++ BetBud/DALBetBud/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace DALBetBud.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DALBetBud.Context.BetBudContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DALBetBud.Context.BetBudContext context)
+        {
+        
+        }
+
+    }
+}
