ad22ef209715168aa725799de731951ddf697237
Reetzer <318956@gmail.com>
2016-04-13 13:20:54 +0200

    jesper fixer fejL! ;)

--- BetBud/ModelLibrary/Bruger/Bruger.cs
+++ BetBud/ModelLibrary/Bruger/Bruger.cs
@@ -5,6 +5,8 @@ namespace ModelLibrary.Bruger
     public class Bruger : IBruger
 
     {
+        public int BrugerId { get; set; }
+
         public string Alias { get; set; }
 
         public string BrugerNavn { get; set; }
