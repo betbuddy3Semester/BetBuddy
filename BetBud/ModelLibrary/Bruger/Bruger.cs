2c2cb2af9fa88af7efd12f17e27e0149183aaa33
zenmasterx <tryharder@live.dk>
2016-04-13 09:28:30 +0200

    Tilføjet reel bruger klasse i stedet for interfacetest

---
+++ BetBud/ModelLibrary/Bruger/Bruger.cs
@@ -0,0 +1,26 @@
+﻿using ModelLibrary.Interface_Bruger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Bruger
+{
+    class Bruger : IBruger 
+    
+      {
+            public string Alias
+            { get; set; }
+
+            public string BrugerNavn
+            { get; set; }
+
+            public string Email
+            { get; set; }
+
+            public string Navn
+            { get; set; }
+        }
+    }
+
