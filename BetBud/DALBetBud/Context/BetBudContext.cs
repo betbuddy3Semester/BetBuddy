834110d772bf68e7c47ad6a32856c8861bad0227
Nickolai <Nickolai.madsen93@gmail.com>
2016-04-13 13:08:04 +0200

    Videre udvikling på context og Entityframework

--- BetBud/DALBetBud/Context/BetBudContext.cs
+++ BetBud/DALBetBud/Context/BetBudContext.cs
@@ -1,14 +1,16 @@
 using System.Data.Entity;
-using BusinessLogicLayer.ApplicationLayer.ModelLayer;
-using WFCLayer;
-
+using ModelLibrary.Bruger;
 namespace DALBetBud.Context
 {
     public class BetBudContext : DbContext
     {
         public BetBudContext() : base("name=BetBudContext")
         {
+            
         }
+
+        public DbSet<Bruger> Brugere { get; set; }
+
 
     }
 
