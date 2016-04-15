dc6c7b7ca342b58e11cce7cb688c528d16a81024
zenmasterx <tryharder@live.dk>
2016-04-15 11:36:03 +0200

    Removed method constructions in BrugerService and moved them to the BrugerController.

---
+++ BetBud/CtrLayer/IBrugerController.cs
@@ -0,0 +1,33 @@
+﻿using ModelLibrary;
+using ModelLibrary.Bruger;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace CtrLayer
+{
+       
+        public interface IBrugerController
+        {
+            //Returner den enkelte bruger ud fra id
+            
+            Bruger getBruger(int id);
+
+            //Returner alle brugere
+            
+            IEnumerable<Bruger> getBrugere();
+
+            
+            void opretBruger(Bruger bruger);
+
+            
+            void opdaterBruger(Bruger bruger);
+
+            
+            void sletBruger(int id);
+
+
+        }
+    }
+}
--- BetBud/WCFBetBuddy/BrugerService.cs
+++ BetBud/WCFBetBuddy/BrugerService.cs
@@ -2,62 +2,42 @@
 using System;
 using System.Linq;
 using DALBetBud.Context;
-
+using CtrLayer;
+using System.Collections.Generic;
 
 namespace WCFBetBuddy
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class BrugerService : IBrugerService
     {
-
-
+        BrugerController BC = new BrugerController();
 
         public Bruger getBruger(int id)
         {
-            using (BetBudContext db = new BetBudContext())
-            {
-
-                return db.Brugere.Find(id);
-
-            }
+            return BC.getBruger(id);
         }
 
-        public System.Collections.Generic.IEnumerable<Bruger> getBrugere()
+        public IEnumerable<Bruger> getBrugere()
         {
-            using (BetBudContext db = new BetBudContext())
-            {
-
-                return db.Brugere.ToList();
-
-            }
+            return BC.getBrugere();
         }
 
         public void opdaterBruger(Bruger bruger)
         {
-            using (BetBudContext db = new BetBudContext())
-            {
-
-                db.Entry(bruger).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-
-            }
+            BC.opdaterBruger(bruger);
         }
 
         public void opretBruger(Bruger bruger)
         {
-            throw new NotImplementedException();
+            BC.opretBruger(bruger);
         }
 
         public void sletBruger(int id)
         {
-            using (BetBudContext db = new BetBudContext())
-            {
-
-                Bruger bruger = new Bruger();
-                bruger.BrugerId = id;
-                db.Entry(bruger).State = System.Data.Entity.EntityState.Deleted;
-                db.SaveChanges();
-            }
+            BC.sletBruger(id);
         }
     }
 }
+
+
+      
