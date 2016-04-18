5937db36568aa609d58b20b3b1e38ab674b5594f
Alex-Pedersen <1038281@ucn.dk>
2016-04-18 09:02:39 +0200

    Fixing the test so it can be run

--- BetBud/BetBudTest/TestKupon/KampTest.cs
+++ BetBud/BetBudTest/TestKupon/KampTest.cs
@@ -1,43 +1,44 @@
 ﻿using System;
-using ModelLibrary.Kupon;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using ModelLibrary.Kupon;
 
 namespace BetBudTest.TestKupon
 {
-    [TestFixture]
     public class KampTest
     {
-        [Test]
-        public void TestAfKampStart()
+        public Kamp kamp1 = new Kamp()
         {
-            var kamp1 = new Kamp
-            {
-                HoldVsHold = "Liverpool - Man UTD",
-                Odds1 = 1.33,
-                OddsX = 3.2,
-                Odds2 = 18.0,
-                Vundet1 = false,
-                VundetX = false,
-                Vundet2 = false,
-                KampStart = new DateTime(),
-                Aflyst = false
-            };
+            HoldVsHold = "Liverpool - Man UTD",
+            Odds1 = 1.33,
+            OddsX = 3.2,
+            Odds2 = 18.0,
+            Vundet1 = false,
+            VundetX = false,
+            Vundet2 = false,
+            KampStart = new DateTime(),
+            Aflyst = false,
+        };
 
-            var kamp2 = new Kamp
-            {
-                HoldVsHold = "Chelsea - Norwich",
-                Odds1 = 1.8,
-                OddsX = 2.8,
-                Odds2 = 3.5,
-                Vundet1 = false,
-                VundetX = false,
-                Vundet2 = false,
-                KampStart = new DateTime(),
-                Aflyst = false,
+        public Kamp kamp2 = new Kamp()
+        {
+            HoldVsHold = "Chelsea - Norwich",
+            Odds1 = 1.8,
+            OddsX = 2.8,
+            Odds2 = 3.5,
+            Vundet1 = false,
+            VundetX = false,
+            Vundet2 = false,
+            KampStart = new DateTime(),
+            Aflyst = false,
+        };
 
-            };
-
-            kamp2.KampStart = kamp2.KampStart.AddDays(2);
+        public KampTest() { 
+        kamp2.KampStart = kamp2.KampStart.AddDays(2);
         }
+
     }
 }
