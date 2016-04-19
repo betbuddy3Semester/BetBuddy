0d4d11e705d54926251918ded493a32f3774f7d3
Reetzer <318956@gmail.com>
2016-04-19 10:16:50 +0200

    Oprettelse af IKuponController og KuponController klasser

---
+++ BetBud/CtrLayer/KuponController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibrary.Kupon;
+
+namespace CtrLayer
+{
+    class KuponController : IKuponController
+    {
+        public bool TilføjKamp(Kamp kamp, bool valgt1, bool valgtX, bool valgt2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FjernKamp(Kamp kamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double OddsUdregning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double MuligGevist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BekræftKupon()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
