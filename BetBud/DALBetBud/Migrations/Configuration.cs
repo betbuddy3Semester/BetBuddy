390119308788a6345412bd2f2f3ba5d49a32fae0
zenmasterx <tryharder@live.dk>
2016-04-21 11:43:51 +0200

    Dropped Db and updated it with new tables

--- BetBud/DALBetBud/Migrations/Configuration.cs
+++ BetBud/DALBetBud/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DALBetBud.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DALBetBud.Context.BetBudContext context)
