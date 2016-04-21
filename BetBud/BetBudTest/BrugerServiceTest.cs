d9b18cc52cee2ea49ee7689c25e62069bc9de3b3
zenmasterx <tryharder@live.dk>
2016-04-21 13:31:24 +0200

    Implemented integration tests for the bruger service region

--- BetBud/BetBudTest/BrugerServiceTest.cs
+++ BetBud/BetBudTest/BrugerServiceTest.cs
@@ -14,10 +14,10 @@ namespace BetBudTest
 
     {
 
-        BrugerServiceRef.BrugerServiceClient BSR = new BrugerServiceRef.BrugerServiceClient();
+        ServiceReference1.ServicesClient BSR = new ServiceReference1.ServicesClient();
 
         /// <summary>
-        ///  Testen er afhængig at servicen manuelt bliver startet, den kan ikke åbne servicen selv 
+        ///  Testen er afhængig af, at servicen manuelt bliver startet, den kan ikke åbne servicen selv 
         /// </summary>
         [Test]
         public void opretBrugerTest()
@@ -34,6 +34,51 @@ namespace BetBudTest
             //Assert
             Bruger b = BSR.getBrugerEfterBrugernavn(bruger.BrugerNavn);
             Assert.AreEqual("LuderLaila", b.BrugerNavn);
+            BSR.sletBruger(b.BrugerId);
+        }
+
+        [Test]
+        public void sletBrugerTest()
+        {
+            //Arrange
+            Bruger bruger = new Bruger();
+
+
+            //Act 
+            bruger.BrugerNavn = "Hans Testies";
+            bruger.Email = "Testies@hehe.fu";
+            BSR.opretBruger(bruger);
+
+            //Assert
+            Bruger b = BSR.getBrugerEfterBrugernavn(bruger.BrugerNavn);
+            Assert.AreEqual("Hans Testies", b.BrugerNavn);
+            BSR.sletBruger(b.BrugerId);
+            
+        }
+
+        [Test]
+        public void opdaterBrugerTest()
+        {
+            //Arrange
+            Bruger bruger = new Bruger();
+
+
+            //Act 
+            bruger.BrugerNavn = "LuderLaila";
+            bruger.Email = "lailasmås@hehe.fu";
+            BSR.opretBruger(bruger);
+
+            //Assert
+            Bruger b = BSR.getBrugerEfterBrugernavn(bruger.BrugerNavn);
+            Assert.AreEqual("LuderLaila", b.BrugerNavn);
+            
+            b.BrugerNavn = "JULIE HAR IKKE BUKSER I HÅRET";
+            b.Email = "BUKSiHÅR@hehe.fu";
+            BSR.opdaterBruger(b);
+
+            Bruger b2 = BSR.getBrugerEfterBrugernavn("JULIE HAR IKKE BUKSER I HÅRET");
+            Assert.AreEqual("JULIE HAR IKKE BUKSER I HÅRET", b2.BrugerNavn);
+            Assert.AreEqual("BUKSiHÅR@hehe.fu", b2.Email);
             BSR.sletBruger(b.BrugerId);
         }
     }
