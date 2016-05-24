57df521b59741000a9436b017273140a780fd1b9
Reetzer <318956@gmail.com>
2016-05-24 13:12:57 +0200

    fuvk

---
+++ BetBud/CtrLayer/ISæsonController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrLayer
+{
+    interface ISæsonController
+    {
+        BrugerController NyBrugerController { get; set; }
+        KuponController NyKuponController { get; set; }
+
+        
+
+        DateTime SæsonStart();
+        DateTime SæsonAfslutning();
+    }
+}
