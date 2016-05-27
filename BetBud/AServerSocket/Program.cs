2dc6e2e67d42c258050e7b600942d1101f484a27
Alex-Pedersen <1038281@ucn.dk>
2016-05-27 12:27:43 +0200

    Adding last reference files

---
+++ BetBud/AServerSocket/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+using ModelLibrary.Chat;
+
+namespace AServerSocket
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AServer serv = new AServer()
+            {
+                ServerPort = 105
+            };
+
+            Client cli = new Client()
+            {
+                ClientPort = 105
+            };
+
+            Console.Title = "Server";
+            serv.StartServer();
+
+            Thread.Sleep(50);
+            cli.ConnectToServer();
+
+            Thread.Sleep(50);
+            cli.SendResponse("Potato");
+
+            Console.ReadLine();
+            serv.StopServer();
+
+            
+        }
+    }
+}
---
+++ BetBud/ModelLibrary/Interfaces/SeasonInterface/ISæsonBruger.cs
@@ -0,0 +1,13 @@
+﻿using ModelLibrary.Models.Bruger;
+
+namespace ModelLibrary.Interfaces.SeasonInterface {
+    public interface ISæsonBruger {
+        #region Properties
+
+        Bruger Bruger { get; set; }
+        double BrugerPoints { get; set; }
+
+        #endregion
+
+    }
+}
